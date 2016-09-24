27768bf9ae48eb5d8cf56a2593b17a901f081741
akamaozu <uzo@designbymobius.ca>
2016-09-24 09:25:16 -0400

    log posted events

--- routes/events/receive-new-event.js
+++ routes/events/receive-new-event.js
@@ -1,8 +1,10 @@
 var receive_new_event_route = module.exports = require('express').Router();
 
+receive_new_event_route.use( require('body-parser')() );
+
 receive_new_event_route.post('/', function( req, res ){
   
-  console.log('event received');
+  req.noticeboard.log( 'NEW EVENT\n', req.body );
 
   res.sendStatus( 200 );
 });
