e1d6eedd2ca8e82cb63f9f934bd688618801296f
akamaozu <uzo@designbymobius.ca>
2016-09-24 10:16:32 -0400

    parse event
    handle pull request events

--- routes/events/receive-new-event.js
+++ routes/events/receive-new-event.js
@@ -3,8 +3,31 @@ var receive_new_event_route = module.exports = require('express').Router();
 receive_new_event_route.use( require('body-parser')() );
 
 receive_new_event_route.post('/', function( req, res ){
-  
-  req.noticeboard.log( 'NEW EVENT\n', req.body );
+
+  // determine event type
+
+    // pull request
+      if( req.body.pullrequest ){
+
+        var pullrequest = req.body.pullrequest;
+
+        req.noticeboard.log( 
+          'NEW PULL REQUEST\n',
+          'author:\n',
+          pullrequest.author,
+          'state: ' + pullrequest.state.toLowerCase() + '\n',
+          'src:\n',
+          pullrequest.source,
+          'dest:\n',
+          pullrequest.destination,
+        );
+      }
+
+    // unknown
+      else{
+
+        req.noticeboard.log( 'NEW EVENT\n', req.body );
+      }  
 
   res.sendStatus( 200 );
 });
