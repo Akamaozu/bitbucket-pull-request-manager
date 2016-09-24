2103822b69e6dae7beb4c284f97d3b3e08f97a27
akamaozu <uzo@designbymobius.ca>
2016-09-24 07:40:54 -0400

    loading root dir and noticeboard as express server variables

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var server = require('express')();
+var server = require('express')(),
+    Noticeboard = require('cjs-noticeboard');
+
+// load server variables
+  server.locals.app_root_dir = __dirname;
+  server.locals.noticeboard = new Noticeboard({ logOps: false });
 
 server.get('/', function( req, res ){
 
