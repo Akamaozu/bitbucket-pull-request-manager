d4a2d479d3b796d158f5729331396aed03f19805
akamaozu <uzo@designbymobius.ca>
2016-09-24 07:50:18 -0400

    load environment variables from .env file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var server = require('express')(),
     Noticeboard = require('cjs-noticeboard');
 
+// load .env
+  require('dotenv').config();
+
 // load server variables
   server.locals.app_root_dir = __dirname;
   server.locals.noticeboard = new Noticeboard({ logOps: false });
