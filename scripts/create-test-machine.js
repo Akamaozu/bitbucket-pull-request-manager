2fee1f6256927173ef6b4d7711bf0869b430923b
akamaozu <uzo@designbymobius.ca>
2016-09-25 07:40:55 -0400

    still debugging heroku fs

--- scripts/create-test-machine.js
+++ scripts/create-test-machine.js
@@ -9,11 +9,11 @@ var shell = require('shelljs'),
 if( !has_ssh ) throw new Error( 'this script requires ssh module to work' );
 if( !has_git ) throw new Error( 'this script requires git module to work' );
 
-console.log( require('fs').readdirSync('/') );
-console.log( require('fs').readdirSync('/etc/.ssh') );
+console.log( require('fs').readdirSync('/etc') );
+// console.log( require('fs').readdirSync('/etc/.ssh') );
 
-require('fs').writeFileSync( '/etc/.ssh/test_machine_rsa.pub', 'fake_rsa.pub' );
-require('fs').writeFileSync( '/etc/.ssh/test_machine_rsa', 'fake_rsa' );
+// require('fs').writeFileSync( '/etc/.ssh/test_machine_rsa.pub', 'fake_rsa.pub' );
+// require('fs').writeFileSync( '/etc/.ssh/test_machine_rsa', 'fake_rsa' );
 
-console.log( require('fs').readdirSync('/') );
-console.log( require('fs').readdirSync('/etc/.ssh') );
+// console.log( require('fs').readdirSync('/') );
+// console.log( require('fs').readdirSync('/etc/.ssh') );
