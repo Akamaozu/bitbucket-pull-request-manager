2319bd55e5abba91983132733a2d727ce4f70144
akamaozu <uzo@designbymobius.ca>
2016-09-25 07:37:04 -0400

    bad idea setting this via cmd line... why not http post?

--- scripts/create-test-machine.js
+++ scripts/create-test-machine.js
@@ -9,64 +9,11 @@ var shell = require('shelljs'),
 if( !has_ssh ) throw new Error( 'this script requires ssh module to work' );
 if( !has_git ) throw new Error( 'this script requires git module to work' );
 
-// required vars
-  var homedir = require('os').homedir();
+console.log( require('fs').readdirSync('/') );
+console.log( require('fs').readdirSync('/etc/.ssh') );
 
-console.log( homedir );
+require('fs').writeFileSync( '/etc/.ssh/test_machine_rsa.pub', 'fake_rsa.pub' );
+require('fs').writeFileSync( '/etc/.ssh/test_machine_rsa', 'fake_rsa' );
 
-// get test machine info
-var prompt = require('prompt'),
-    machine = {
-
-      name: {
-        pattern: /^[a-zA-Z\-]+$/,
-        message: 'Only letters or dashes permitted',
-        required: true
-      },
-
-      git: {
-        message: 'Test Machine\'s Git URL',
-        required: true
-      },
-
-      git_active_branch: {
-        message: 'Test Machine\'s Checked-Out Branch',
-        required: true
-      },
-
-      ssh_public_key: {
-        message: 'Test Machine\'s pubkey. Must be on Bitbucket as well',
-        hidden: true,
-        required: true
-      },
-
-      ssh_private_key: {
-        message: 'Test Machine\'s private key. Guard this jealously',
-        hidden: true,
-        required: true
-      }
-    };
-
-prompt.start();
-
-prompt.get({ properties: machine }, function( error, machine_props ){
-
-  if( error ) throw error;
-
-  shell.mkdir( '-p' , homedir + '/.ssh');
-
-  console.log( require('fs').readdirSync(homedir + '') );
-  console.log( require('fs').readdirSync(homedir + '/.ssh') );
-
-  require('fs').writeFileSync( homedir + '/.ssh/test_machine_rsa.pub', machine_props.ssh_public_key );
-  require('fs').writeFileSync( homedir + '/.ssh/test_machine_rsa', machine_props.ssh_private_key );
-
-  var bitbucket_ssh = require('ssh-connect-prompt')( process.env.BITBUCKET_REPO );
-
-  bitbucket_ssh.on('ready', function(){
-
-    console.log( 'everything is going according to plan' );
-  });
-});
-
-
+console.log( require('fs').readdirSync('/') );
+console.log( require('fs').readdirSync('/etc/.ssh') );
