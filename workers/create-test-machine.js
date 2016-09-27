c32aef403fca829087cd036253b52c26f25811df
akamaozu <uzo@designbymobius.ca>
2016-09-27 18:59:16 -0400

    git version too low for custom ssh command .. trying config file

--- workers/create-test-machine.js
+++ workers/create-test-machine.js
@@ -24,18 +24,20 @@ rabbitmq.handle( 'create-test-machine', function( token, ack, nack ){
     fs.writeFileSync( process.env.HOME + '/.ssh/test_machine_rsa', machine.ssh_priv_key );
     shell.chmod( 400, process.env.HOME + '/.ssh/test_machine_rsa' );
 
+    // add private key to ssh config
+    fs.writeFileSync( process.env.HOME + '/.ssh/config', "Host *" + require('os').EOL + "IdentityFile ~/.ssh/test_machine_rsa" );
+
     // create fake repo dir
     shell.mkdir( '-p', process.env.HOME + '/test-repo' );
 
     // create fake repo and add remotes
     shell.exec( 'git init && git remote add test-machine ' + machine.git + ' && git remote add bitbucket ' + process.env.BITBUCKET_REPO, { cwd: process.env.HOME + '/test-repo' });
-    shell.exec( 'git --version', { cwd: process.env.HOME + '/test-repo' });
 
     // see if remotes were added
     shell.exec( 'git remote -v', { cwd: process.env.HOME + '/test-repo' });
 
     // test ssh connection to remotes
-    shell.exec( 'GIT_SSH_COMMAND="ssh -i ' + process.env.HOME + '/.ssh/test_machine_rsa" git ls-remote bitbucket', { cwd: process.env.HOME + '/test-repo' });
+    shell.exec( process.env.HOME + '/.ssh/test_machine_rsa" git ls-remote bitbucket', { cwd: process.env.HOME + '/test-repo' });
 
     nack();
     process.exit();
