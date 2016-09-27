5e2694ef7fb1181a98860101f466fdfadc36a450
akamaozu <uzo@designbymobius.ca>
2016-09-27 17:24:05 -0400

    debugging heroku dyno ssh config

---
+++ workers/create-test-machine.js
@@ -0,0 +1,31 @@
+var rabbitmq = require('../utils/rabbitmq-helper')( process.env.RABBITMQ_URL ),
+    jwt = require('../utils/jwt-helper')( process.env.TEST_MACHINE_CREATION_SECRET );
+
+rabbitmq.create( 'create-test-machine' );
+
+rabbitmq.handle( 'create-test-machine', function( token, ack, nack ){
+  
+  jwt.verify( token, function( error, content ){
+
+    if( error ){
+
+      nack();
+      return process.exit();
+    }
+
+    var machine = content,
+        fs = require('fs'),
+        shell = require('shelljs');
+
+    fs.writeFileSync( '/etc/ssh/test_machine_rsa', machine.ssh_priv_key );
+    shell.chmod( 400, '/etc/ssh/test_machine_rsa' );
+
+
+    console.log( fs.readdirSync( '/etc/ssh' ) );
+    console.log( fs.readFileSync( '/etc/ssh/ssh_config' ) );
+    console.log( fs.readFileSync( '/etc/ssh/known_hosts' ) );
+
+    nack();
+    process.exit();
+  });
+});
